5fcfec6754b0b7d4fafd44c6e78b871af729e529
Darrell Mayson <mcdoob@gmail.com>
2017-06-20 03:09:32 +1200

    add contact data to google spreadsheet

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,7 +11,7 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(secure_params)
     if @contact.valid?
-      # TODO save data
+      @contact.update_spreadsheet # save details to google_spreadsheet
       # TODO send message
       flash[:notice] = "Message sent from #{@contact.name}."
       redirect_to root_path
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -14,4 +14,20 @@ class Contact < ActiveRecord::Base
     :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i
   validates_length_of :content, :maximum => 500
 
+  def update_spreadsheet
+    # Authenticate a session with your Service Account
+    session = GoogleDrive::Session.from_service_account_key("client_secret.json")
+     
+    # Get the spreadsheet by its title
+    spreadsheet = session.spreadsheet_by_title("learn-rails")
+    # Get the first worksheet
+    worksheet = spreadsheet.worksheets.first
+    last_row = 1 + worksheet.num_rows
+    worksheet[last_row, 1] = Time.new
+    worksheet[last_row, 2] = self.name
+    worksheet[last_row, 3] = self.email
+    worksheet[last_row, 4] = self.content
+    worksheet.save
+  end
+
 end
